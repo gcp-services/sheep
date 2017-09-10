14a92625b8305a1eaba63aefe4ef19233fc72c58
Antonio Lobato <antonio@whisper.sh>
2017-09-10 00:49:51 -0700

    switch to glide, add makefile

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	go build
+	go install
+
+run:
+	${GOPATH}/bin/sheep
--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"github.com/labstack/echo"
+)
+
 func main() {
+	e := echo.New()
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.String(200, "ok")
+	})
+	e.HideBanner = true
+
+	e.Logger.Fatal(e.Start(":5309"))
 }
