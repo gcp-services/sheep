60a971b38717ec7438018615b9950ad7380b58c1
Antonio Lobato <antonio@whisper.sh>
2019-01-15 08:14:49 -0800

    more tests

--- api/v1/api_test.go
+++ api/v1/api_test.go
@@ -15,7 +15,7 @@ func TestGetWithNotFound(t *testing.T) {
 		Name:     "test",
 	}
 
-	db, err := database.NewMockDatabase()
+	db, err := database.NewMockDatabase(false)
 	assert.Nil(t, err)
 	api := &API{
 		Database: db,
@@ -35,7 +35,7 @@ func TestGetDirect(t *testing.T) {
 		Operation: Counter_INCR,
 		Direct:    true,
 	}
-	db, err := database.NewMockDatabase()
+	db, err := database.NewMockDatabase(false)
 	assert.Nil(t, err)
 	api := &API{
 		Database: db,
@@ -47,4 +47,88 @@ func TestGetDirect(t *testing.T) {
 	res, err = api.Get(context.Background(), in)
 	assert.Nil(t, err)
 	assert.Equal(t, int64(1), res.GetValue())
+}
+
+func TestGetStream(t *testing.T) {
+	in := &Counter{
+		Keyspace:  "test",
+		Key:       "test",
+		Name:      "test",
+		Uuid:      "123",
+		Operation: Counter_INCR,
+		Direct:    false,
+	}
+
+	db, err := database.NewMockDatabase(false)
+	assert.Nil(t, err)
+
+	stream, err := database.NewMockQueue(false)
+	assert.Nil(t, err)
+
+	stream.StartWork(db)
+
+	api := &API{
+		Database: db,
+		Stream:   stream,
+	}
+
+	res, err := api.Update(context.Background(), in)
+	assert.Nil(t, err)
+	assert.Equal(t, "", res.GetError())
+
+	res, err = api.Get(context.Background(), in)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), res.GetValue())
+}
+
+func TestGetError(t *testing.T) {
+	in := &Counter{
+		Keyspace:  "test",
+		Key:       "test",
+		Name:      "test",
+		Uuid:      "123",
+		Operation: Counter_INCR,
+		Direct:    false,
+	}
+
+	db, err := database.NewMockDatabase(true)
+	assert.Nil(t, err)
+
+	stream, err := database.NewMockQueue(false)
+	assert.Nil(t, err)
+
+	stream.StartWork(db)
+	api := &API{
+		Database: db,
+		Stream:   stream,
+	}
+	res, err := api.Update(context.Background(), in)
+	assert.Nil(t, err)
+	assert.Equal(t, "", res.GetError())
+
+	res, err = api.Get(context.Background(), in)
+	assert.NotNil(t, err)
+	assert.Equal(t, int64(0), res.GetValue())
+}
+
+func TestUpdateError(t *testing.T) {
+	in := &Counter{
+		Keyspace:  "test",
+		Key:       "test",
+		Name:      "test",
+		Uuid:      "123",
+		Operation: Counter_INCR,
+		Direct:    true,
+	}
+
+	db, err := database.NewMockDatabase(true)
+	assert.Nil(t, err)
+
+	api := &API{
+		Database: db,
+	}
+
+	res, err := api.Update(context.Background(), in)
+	assert.NotNil(t, err)
+	assert.Equal(t, "", res.GetError())
 }
--- database/mock.go
+++ database/mock.go
@@ -10,33 +10,41 @@ import (
 )
 
 type MockDatabase struct {
-	db     map[string]int64
-	exists map[string]bool
-	log    map[string]bool
-	lock   sync.Mutex
+	ForceError bool
+	db         map[string]int64
+	exists     map[string]bool
+	log        map[string]bool
+	lock       sync.Mutex
 }
 
 type MockQueue struct {
-	queue []*Message
-	c     chan bool
+	ForceError bool
+	queue      []*Message
+	c          chan bool
 }
 
-func NewMockDatabase() (Database, error) {
+func NewMockDatabase(forceError bool) (Database, error) {
 	return &MockDatabase{
-		db:     make(map[string]int64),
-		exists: make(map[string]bool),
-		log:    make(map[string]bool),
-		lock:   sync.Mutex{},
+		ForceError: forceError,
+		db:         make(map[string]int64),
+		exists:     make(map[string]bool),
+		log:        make(map[string]bool),
+		lock:       sync.Mutex{},
 	}, nil
 }
 
-func NewMockQueue() (Stream, error) {
+func NewMockQueue(forceError bool) (Stream, error) {
 	return &MockQueue{
-		c: make(chan bool),
+		ForceError: forceError,
+		c:          make(chan bool),
 	}, nil
 }
 
 func (db *MockDatabase) Save(m *Message) error {
+	if db.ForceError {
+		return db.SaveError(m)
+	}
+
 	db.lock.Lock()
 	defer db.lock.Unlock()
 	if db.log[m.UUID] {
@@ -61,6 +69,9 @@ func (db *MockDatabase) Save(m *Message) error {
 }
 
 func (db *MockDatabase) Read(m *Message) error {
+	if db.ForceError {
+		return db.ReadError(m)
+	}
 	db.lock.Lock()
 	defer db.lock.Unlock()
 	if !db.exists[m.Keyspace+m.Key+m.Name] {
@@ -70,13 +81,27 @@ func (db *MockDatabase) Read(m *Message) error {
 	return nil
 }
 
+func (db *MockDatabase) SaveError(m *Message) error {
+	return &spanner.Error{Code: codes.Internal}
+}
+
+func (db *MockDatabase) ReadError(m *Message) error {
+	return &spanner.Error{Code: codes.Internal}
+}
+
 func (q *MockQueue) Save(m *Message) error {
+	if q.ForceError {
+		q.SaveError(m)
+	}
 	q.queue = append(q.queue, m)
 	q.c <- true
 	return nil
 }
 
 func (q *MockQueue) Read(ctx context.Context, fn MessageFn) error {
+	if q.ForceError {
+		return q.ReadError(ctx, fn)
+	}
 	select {
 	case <-q.c:
 		go func() {
@@ -89,6 +114,14 @@ func (q *MockQueue) Read(ctx context.Context, fn MessageFn) error {
 		}()
 	}
 	return nil
+}
+
+func (q *MockQueue) SaveError(m *Message) error {
+	return &spanner.Error{Code: codes.Internal}
+}
+
+func (q *MockQueue) ReadError(ctx context.Context, fn MessageFn) error {
+	return &spanner.Error{Code: codes.Internal}
 }
 
 // TODO: implement cancel channel
